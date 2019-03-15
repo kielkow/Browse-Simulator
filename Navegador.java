4f76879139f65ae62d9c5d9836ec4d8999fa0eec
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-03-15 00:41:51 -0300

    Add files via upload

---
+++ Navegador.java
@@ -0,0 +1,16 @@
+package navegador;
+
+
+public class Navegador {
+
+
+    public static void main(String[] args) {
+        Pilha p = new Pilha(5);
+        
+        p.push("batata");
+        
+        System.out.println(p.sites[0]);
+        
+    }
+    
+}
