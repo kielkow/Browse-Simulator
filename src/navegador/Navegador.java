fd27e0480edd5f1e96c9ebcc0112f65513123d1a
Matheus <Matheus@Matheus-PC>
2019-03-16 18:06:53 -0300

    Navegador

---
+++ src/navegador/Navegador.java
@@ -0,0 +1,11 @@
+package navegador;
+
+
+public class Navegador {
+
+
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ src/navegador/Pilha.java
@@ -0,0 +1,56 @@
+package navegador;
+
+import java.util.EmptyStackException;
+
+public class Pilha implements Interface{
+    
+    public String sites[];
+    public int topo;
+
+    public Pilha(int tamanho) {
+        sites = new String[tamanho];
+        topo = -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return topo == -1;
+    }
+
+    @Override
+    public boolean isFull() {
+        return topo == sites.length - 1;
+    }
+
+    @Override
+    public int size() {
+        return topo + 1;
+    }
+
+    @Override
+    public void push(String elemento) {
+        if (!isFull()) {
+            topo++;
+            sites[topo] = elemento;
+        }
+    }
+
+    @Override
+    public String pop() throws EmptyStackException {
+        if (!isEmpty()) {
+            String retorno = sites[topo];
+            topo--;
+            return retorno;
+        }
+        throw new EmptyStackException();
+    }
+
+    @Override
+    public String top() throws EmptyStackException {
+        if (!isEmpty()) {
+            return sites[topo];
+        }
+        throw new EmptyStackException();
+    }
+    
+}
